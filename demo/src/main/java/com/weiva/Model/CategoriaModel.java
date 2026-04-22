5a67c3e59b02bcd3289e5fd1256632c2dcc162f2
Felp <wtffelp@gmail.com>
2026-04-22 18:37:12 -0300

    CategoriaModel

---
+++ demo/src/main/java/com/weiva/Model/CategoriaModel.java
@@ -0,0 +1,37 @@
+package com.weiva.Model;
+
+public class CategoriaModel {
+    private int id;
+    private String nome;
+    private String descricao;
+
+    public CategoriaModel(int id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
