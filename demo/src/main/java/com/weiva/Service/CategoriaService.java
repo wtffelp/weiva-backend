98db93ea57bb65c3334a89e0614e6447b83d6b36
Felp <wtffelp@gmail.com>
2026-04-22 18:43:01 -0300

    feat: CategoriaService

---
+++ demo/src/main/java/com/weiva/Service/CategoriaService.java
@@ -0,0 +1,29 @@
+package com.weiva.Service;
+
+import com.weiva.Model.CategoriaModel;
+import com.weiva.Repository.CategoriaRepository;
+
+public class CategoriaService {
+    CategoriaRepository categoriaRepository = new CategoriaRepository();
+
+    public CategoriaModel criarCategoria(String nome, String descricao) {
+        if (buscarPorNome(nome) == null) {
+            return categoriaRepository.criarCategoria(nome, descricao);
+        } else {
+            throw new RuntimeException("Categoria ja cadastrada.");
+        }
+    }
+
+    public CategoriaModel buscarPorId(int id) {
+        return categoriaRepository.buscarPorId(id);
+    }
+
+    public CategoriaModel buscarPorNome(String nome) {
+        return categoriaRepository.buscarPorNome(nome);
+    }
+
+    public CategoriaModel deletarCategoria(int id) {
+        return categoriaRepository.deletarCategoria(id);
+    }
+    
+}
