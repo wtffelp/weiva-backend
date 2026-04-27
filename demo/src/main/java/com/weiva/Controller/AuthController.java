0539e9e44ae757c410642664d536bd01c028464d
Felp <wtffelp@gmail.com>
2026-04-27 20:24:38 -0300

    fix: apply exclusion strategy to hide password in AuthController

--- demo/src/main/java/com/weiva/Controller/AuthController.java
+++ demo/src/main/java/com/weiva/Controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.weiva.Controller;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.weiva.Annotations.AnnotationExclusionStrategy;
 import com.weiva.JWT.JWTService;
 import com.weiva.Model.UserModel;
 import com.weiva.Service.UserService;
@@ -11,7 +13,8 @@ import io.javalin.Javalin;
 public class AuthController {
     private UserService userService = new UserService();
     private JWTService jwtService = new JWTService();
-    private Gson gson = new Gson();
+    private Gson gson = new GsonBuilder()
+    .setExclusionStrategies(new AnnotationExclusionStrategy()).create();
 
     public void authRoutes(Javalin app) {
         app.post("/auth/login", ctx -> {
