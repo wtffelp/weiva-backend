1ce40769ccf4af385cec27fef9631966d7e305fc
Felp <wtffelp@gmail.com>
2026-04-27 08:37:32 -0300

    feat: support environment variables for database config

--- demo/src/main/java/com/weiva/Config/Database.java
+++ demo/src/main/java/com/weiva/Config/Database.java
@@ -11,18 +11,24 @@ public class Database {
 
     public static Jdbi getJdbi(){
         if (jdbi == null) {
-            try{
-                Properties props = new Properties();
-                InputStream input = Database.class.getClassLoader().getResourceAsStream("db.properties");
-                props.load(input);
-                String url = props.getProperty("db.url");
-                String user = props.getProperty("db.user");
-                String password = props.getProperty("db.password");
-                jdbi = Jdbi.create(url, user, password);
-            } catch (IOException e) {
-                System.out.println("Erro ao conectar com o banco: " + e.getMessage());
+
+            String url      = System.getenv("DB_URL");
+            String user     = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
+            
+            if (url == null){
+                try{
+                    Properties props = new Properties();
+                    InputStream input = Database.class.getClassLoader().getResourceAsStream("db.properties");
+                    props.load(input);
+                    url = props.getProperty("db.url");
+                    user = props.getProperty("db.user");
+                    password = props.getProperty("db.password");
+                } catch (IOException e) {
+                    System.out.println("Erro ao conectar com o banco: " + e.getMessage());
+                }
             }
-            return jdbi;
+            jdbi = Jdbi.create(url, user, password);
         }
         return jdbi;
     }
